ec565cf30098df708ec11e96f64666607a522fcd
GABrambila <brambila.gabriel26@gmail.com>
2023-10-19 19:42:10 -0300

    inicial

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.header}>LISTAS</Text>
+      <Button title="Animais" onPress={() => console.log('Animais')} />
+      <Button title="Carros" onPress={() => console.log('Carros')} />
+      <Button title="Peixes" onPress={() => console.log('Peixes')} />
+      <Button title="Times" onPress={() => console.log('Times')} />
+      <Button title="Filmes" onPress={() => console.log('Filmes')} />
+      <Button title="Linguagens" onPress={() => console.log('Linguagens')} />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
 });
+
+export default App;
